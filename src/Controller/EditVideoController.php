865935e1ab78b1040d7e67efac6c81fa3a5d14f0
Hellen Bianchini <hellenbiangod@gmail.com>
2025-04-11 11:15:51 -0300

    seguranca

--- src/Controller/EditVideoController.php
+++ src/Controller/EditVideoController.php
@@ -35,12 +35,19 @@ class EditVideoController implements Controller
         $video = new Video($url, $titulo);
         $video->setId($id);
         if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
-            move_uploaded_file(
-                $_FILES['image']['tmp_name'],
-                __DIR__ . '/../../public/img/uploads/' . $_FILES['image']['name']
-            );
+            $finfo = new \finfo(FILEINFO_MIME_TYPE);
+            $mimeType = $finfo->file($_FILES['image']['tmp_name']);
 
-            $video->setFilePath($_FILES['image']['name']);
+            if (str_starts_with($mimeType, 'image/')) {
+                $safeFileName = uniqid('upload_') . '_' . pathinfo($_FILES['image']['name'], PATHINFO_BASENAME);
+
+                move_uploaded_file(
+                    $_FILES['image']['tmp_name'],
+                    __DIR__ . '/../../public/img/uploads/' . $safeFileName
+                );
+
+                $video->setFilePath($safeFileName);
+            }
         }
 
         $success = $this->videoRepository->update($video);
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -20,6 +20,13 @@ class LoginController implements Controller
         $stmt->execute();
         $userData = $stmt->fetch(PDO::FETCH_ASSOC);
         $correctPassword = password_verify($password, $userData['password'] ?? '');
+        if (password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)) {
+            $stmt = $this->pdo->prepare('UPDATE users SET password = ? WHERE id = ?');
+            $stmt->bindValue(1, password_hash($password, PASSWORD_ARGON2ID));
+            $stmt->bindValue(2, $userData['id']);
+
+        }
+
         if ($correctPassword) {
             $_SESSION['logado'] = true;
             header('Location: /');
