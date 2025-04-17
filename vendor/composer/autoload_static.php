3b35f0dada6649770467cac550d78c8037697eeb
Hellen Bianchini <hellenbiangod@gmail.com>
2025-04-17 13:41:33 -0300

    plates

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -24,6 +24,7 @@ class ComposerStaticInitf88c5f177dbc0a283bf791e8521553b7
         ),
         'L' => 
         array (
+            'League\\Plates\\' => 14,
             'Laravel\\SerializableClosure\\' => 28,
         ),
         'I' => 
@@ -61,6 +62,10 @@ class ComposerStaticInitf88c5f177dbc0a283bf791e8521553b7
         'Nyholm\\Psr7Server\\' => 
         array (
             0 => __DIR__ . '/..' . '/nyholm/psr7-server/src',
+        ),
+        'League\\Plates\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/league/plates/src',
         ),
         'Laravel\\SerializableClosure\\' => 
         array (
