99efa4bba6b7c343c0d73fec1512e1c83099f66b
Hellen Bianchini <hellenbiangod@gmail.com>
2025-04-17 13:13:17 -0300

    Configurando as dependências

--- config/di.php
+++ config/di.php
@@ -1,9 +1,15 @@
 <?php
 
+use Alura\Mvc\Repository\VideoRepository;
 use DI\ContainerBuilder;
 
 $builder = new ContainerBuilder();
-
+$builder->addDefinitions([
+    PDO::class => function (): PDO {
+        $dbPath = __DIR__ . '/../banco.sqlite';
+        return new PDO("sqlite:$dbPath");
+    },
+]);
 
 $container = $builder->build();
 
--- public/index.php
+++ public/index.php
@@ -1,24 +1,14 @@
 <?php
 
 declare(strict_types=1);
-use Alura\Mvc\Controller\DeleteVideoController;
-use Alura\Mvc\Controller\EditVideoController;
+
 use Alura\Mvc\Controller\Error404Controller;
-use Alura\Mvc\Controller\NewVideoController;
-use Alura\Mvc\Controller\VideoFormController;
-use Alura\Mvc\Controller\VideoListController;
-use Alura\Mvc\Controller\LoginController;
-use Alura\Mvc\Repository\VideoRepository;
+
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$dbPath = __DIR__ . '/../banco.sqlite';
-$pdo = new PDO("sqlite:$dbPath");
-
-$videoRepository = new VideoRepository($pdo);
-
 $routes = require_once __DIR__ . '/../config/routes.php';
-$dependencyContainer = require_once  __DIR__ . '/../config/di.php';
+/** @var \Psr\Container\ContainerInterface $dependencyContainer */
+$dependencyContainer = require_once __DIR__ . '/../config/di.php';
 $pathInfo = $_SERVER['PATH_INFO'] ?? '/';
 $httpmethod = $_SERVER['REQUEST_METHOD'];
 
